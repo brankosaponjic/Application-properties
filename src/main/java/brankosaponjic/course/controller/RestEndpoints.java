6cda6d1c47e2b7750b7652884fcb0488622dd0d7
Branko <brankosaponjic76@gmail.com>
2021-03-17 16:54:43 +0100

    Added CourseConfiguration and public getHierarchical method in RestEndpoints

--- src/main/java/brankosaponjic/course/controller/RestEndpoints.java
+++ src/main/java/brankosaponjic/course/controller/RestEndpoints.java
@@ -1,8 +1,12 @@
 package brankosaponjic.course.controller;
 
+import brankosaponjic.course.configuration.CourseConfiguration;
 import brankosaponjic.course.model.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class RestEndpoints {
@@ -13,11 +17,26 @@ public class RestEndpoints {
     @Value("${default.course.chapterCount}")
     private int courseChapterCounter;
 
+    @Autowired
+    private CourseConfiguration configuration;
+
     @RequestMapping("/defaultCourse")
     public Course getDefaultCourse(
             @RequestParam(value = "name", defaultValue = "Spring Boot", required = false) String name,
             @RequestParam(value = "chapterCount", defaultValue = "2", required = false) int chapterCount) {
         return new Course(courseName, courseChapterCounter);
+    }
+
+    @RequestMapping("/getHierarchical")
+    public HashMap<String, Object> getConfigAnnotateProperties() {
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", configuration.getName());
+        map.put("characterCount", configuration.getChapterCount());
+        map.put("rating", configuration.getRating());
+        map.put("author", configuration.getAuthor());
+
+        return map;
     }
 
     @RequestMapping("/course")
